12c9637ebccb76aae6b16376b036f50323747422
jungahshin <shinasong@naver.com>
2019-12-17 21:52:30 +0900

    router 수정

--- routes/blogs/articles/articles.js
+++ routes/blogs/articles/articles.js
@@ -6,7 +6,9 @@ const resMessage = require('../../../module/responseMessage');
 const authUtil = require("../../../module/authUtils");//토큰
 const articles = require('../../../model/articles');
 
-// [GET]/blogs/${blogIdx}/articles [POST]/blogs/${blogIdx}/articles [PUT]/blogs/${blogIdx}/articles [DELETE]/blogs/${blogIdx}/articles
+// [GET]/blogs/${blogIdx}/articles [POST]/blogs/${blogIdx}/articles 
+// [PUT]/blogs/${blogIdx}/articles [DELETE]/blogs/${blogIdx}/articles
+//특정 블로그(blogIdx)에 article추가
 
 router.get('/', async(req, res)=>{
     
